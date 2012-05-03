e9df485051b8dc71e317b221604075c5d09f1ef8
k0zmo <koozmoo@gmail.com>
2012-05-03 12:44:09 +0200

    * Drobne zmiany w UI

--- morph2/main.cpp
+++ morph2/main.cpp
@@ -1,14 +1,33 @@
 #include <QApplication>
-#include <QCleanlooksStyle>
+#include <QStyleFactory>
 
+#include <QSettings>
+#include <cstdlib>
 #include "controller.h"
+
+#ifdef _WIN32
+#define setenv _putenv_s
+#endif
 
 int main(int argc, char *argv[])
 {
 	QApplication::setAttribute(Qt::AA_X11InitThreads);
-	QApplication::setStyle(new QCleanlooksStyle);
-
 	QApplication a(argc, argv);
+
+	QStringList styles = QStyleFactory::keys();
+	if(styles.contains("QtCurve"))
+	{
+		QSettings s("./settings.cfg", QSettings::IniFormat);
+		QString env = s.value("gui/qtcurvestyle").toString();
+		QByteArray data = env.toAscii();
+
+		setenv("QTCURVE_CONFIG_FILE", data.constData());
+
+		QStyle* style = QStyleFactory::create("QtCurve");
+		if(style)
+			QApplication::setStyle(style);
+	}
+
 	Controller c;
 	c.start();
 
